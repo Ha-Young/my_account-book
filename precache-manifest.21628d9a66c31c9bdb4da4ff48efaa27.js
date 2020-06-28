b891df3269d0ef330be26d195d78a46b1c92b8b7
hychoi <hayeong28@naver.com>
2020-06-28 17:30:15 +0900

    Updates

---
+++ precache-manifest.21628d9a66c31c9bdb4da4ff48efaa27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "044c7b7e09900d83115b1bb22fb0a143",
+    "url": "/my_account-book/index.html"
+  },
+  {
+    "revision": "1b8bd4d7380ff73bf68d",
+    "url": "/my_account-book/static/js/2.fa0e68bc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my_account-book/static/js/2.fa0e68bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f5bfaeb69468a897c4e",
+    "url": "/my_account-book/static/js/main.e149d464.chunk.js"
+  },
+  {
+    "revision": "40f675985935d4b8872f",
+    "url": "/my_account-book/static/js/runtime-main.b07e65a6.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/my_account-book/precache-manifest.21628d9a66c31c9bdb4da4ff48efaa27.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my_account-book/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
